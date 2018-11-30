08817680ec7611e5be150f08fa6e6aa00c5f0b59
sdevine188 <sdevine188@gmail.com>
2018-11-30 17:19:50 -0500

    updated

--- test_means.R
+++ test_means.R
@@ -29,9 +29,11 @@ library(tidyverse)
 library(broom)
 library(dplyr)
 
+# note that unweighted t.test with var.equal = TRUE will match ols dummy t.test regression
 
 # create test_means function to use wtd.t.test (numeric) or prop.test (proportion) to test difference in means
-test_means <- function(x, y, weights_x = NULL, weights_y = NULL, weights_are_statistical_not_sample_size = TRUE, yates_correction = FALSE) {
+test_means <- function(x, y, weights_x = NULL, weights_y = NULL, 
+                       weights_are_statistical_not_sample_size = TRUE, yates_correction = FALSE, var.equal = FALSE) {
         
         # check weights_x
         if(is.null(weights_x)) {
@@ -63,46 +65,36 @@ test_means <- function(x, y, weights_x = NULL, weights_y = NULL, weights_are_sta
         ##########################################################################################
         
         
-        # check if x and y are dummies (only 0/1 values), and if there are no weights (ie all weights are same value)
-        # if so, use prop.test; if x and y are dummies but there are weights, will use wtd.t.test
-        if(length(unique(x %in% c(1, 0, NA))) == 1 & !(FALSE %in% unique(x %in% c(1, 0, NA))) & 
-           length(unique(y %in% c(1, 0, NA))) == 1 & !(FALSE %in% unique(y %in% c(1, 0, NA))) &
-           length(unique(weights_x)) == 1 & length(unique(weights_y)) == 1) {
-                
-                # using prop.test to test significant difference in proportions
-                # http://www.sthda.com/english/wiki/two-proportions-z-test-in-r
-                
-                # without yates continuity correction (default) - note this is same as ols and manual
-                # sthda.com says "yates correction is really important if either the expected successes or failures is < 5"
-                # although multiple sources say it's common advice to not use yates correction at all anymore because it overcorrects, despite base r prop.test default
-                # https://www.statisticshowto.datasciencecentral.com/what-is-the-yates-correction/
+        # if neither x or y has weights use t.test
+        # note that unweighted t.test with var.equal = TRUE will match ols dummy t.test regression
+        if(length(unique(weights_x)) == 1 & length(unique(weights_y)) == 1) {
                 
                 # get test_type
-                test_type <- "prop.test"
-                               
+                test_type <- "t.test"
+                
                 # get flag for whether weights were used
                 weights_x_used <- ifelse(length(unique(weights_x)) == 1, FALSE, TRUE)
                 weights_y_used <- ifelse(length(unique(weights_y)) == 1, FALSE, TRUE)
-                 
-                # get count of successes
-                x_count_successes <- length(x[x == 1])
-                x_length <- length(x)
-
-                y_count_successes <- length(y[y == 1])
-                y_length <- length(y)
-
-                # call prop.test
-                summary_tbl <- prop.test(x = c(x_count_successes, y_count_successes), n = c(x_length, y_length), 
-                                         alternative = "two.sided", correct = yates_correction) %>% 
-                        tidy() %>% mutate(diff_in_means = estimate1 - estimate2, std_error = (conf.high - diff_in_means) / 1.96) %>%
-                        rename(mean_x = "estimate1", mean_y = "estimate2", t_stat = "statistic",
-                                          p_value = "p.value", conf_int_lower = "conf.low", conf_int_upper = "conf.high") %>%
-                        mutate(test_type = test_type, weights_x_used = weights_x_used, weights_y_used = weights_y_used,
+                
+                # call wtd.t.test
+                summary <- t.test(x = x, y = y, alternative = "two.sided", var.equal = var.equal) %>% tidy()
+                
+                # if var.equal = TRUE, t.test output doesn't provide diff_in_means (called "estimate"), so create it manually
+                if(var.equal == TRUE) {
+                        summary <- summary %>% mutate(estimate = estimate1 - estimate2)
+                }
+                
+                # create tidy summary_tbl with results
+                summary_tbl <- summary %>% rename(p_value = "p.value", t_stat = "statistic", diff_in_means = "estimate", mean_x = "estimate1",
+                                    mean_y = "estimate2", conf_int_upper = "conf.high", conf_int_lower = "conf.low") %>% 
+                        mutate(std_error = (conf_int_upper - diff_in_means) / 2, test_type = test_type, 
+                               weights_x_used = weights_x_used, weights_y_used = weights_y_used,
                                x_unweighted_count = length(x[!is.na(x)]), x_na_count = sum(is.na(x)),
-                               y_unweighted_count = length(y[!is.na(y)]), y_na_count = sum(is.na(y))) %>%
+                               y_unweighted_count = length(y[!is.na(y)]), y_na_count = sum(is.na(y)),
+                               equal_variance = var.equal) %>%
                         select(x_unweighted_count, x_na_count, y_unweighted_count, y_na_count, 
                                mean_x, mean_y, diff_in_means, weights_x_used, weights_y_used, 
-                               test_type, t_stat, p_value, std_error, conf_int_lower, conf_int_upper)
+                               test_type, t_stat, p_value, std_error, equal_variance, conf_int_lower, conf_int_upper)
                 return(summary_tbl)
         }
         
@@ -110,10 +102,8 @@ test_means <- function(x, y, weights_x = NULL, weights_y = NULL, weights_are_sta
         ##########################################################################################
         
         
-        # check to ensure x and y are not unweighted dummies (would use prop.test in that case)
-        if(!(length(unique(x %in% c(1, 0, NA))) == 1 & !(FALSE %in% unique(x %in% c(1, 0, NA))) & 
-             length(unique(y %in% c(1, 0, NA))) == 1 & !(FALSE %in% unique(y %in% c(1, 0, NA))) &
-             length(unique(weights_x)) == 1 & length(unique(weights_y)) == 1)) {
+        # if x or y has weights other than 1, use wtd.t.test
+        if(length(unique(weights_x)) != 1 | length(unique(weights_y)) != 1) {
                 
                 # get test_type
                 test_type <- "wtd.t.test"
@@ -131,21 +121,21 @@ test_means <- function(x, y, weights_x = NULL, weights_y = NULL, weights_are_sta
                         spread(key = var_names, value = coefficients)
                 additional_tbl <- tibble(var_names = names(summary$additional), values = summary$additional) %>% 
                         spread(key = var_names, value = values)
-                summary_tbl <- bind_cols(coefficients_tbl, additional_tbl) %>% rename(degrees_of_freedom = "df",
-                                                      p_value = "p.value", t_stat = "t.value", diff_in_means = "Difference",
-                                                      mean_x = "Mean.x", mean_y = "Mean.y", std_error = "Std. Err") 
+                summary_tbl <- bind_cols(coefficients_tbl, additional_tbl) %>% rename(p_value = "p.value", t_stat = "t.value", diff_in_means = "Difference",
+                                                                                      mean_x = "Mean.x", mean_y = "Mean.y", std_error = "Std. Err") 
                 
                 # get conf_int
-                summary_tbl <- summary_tbl %>% mutate(conf_int_lower = diff_in_means - (1.96 * std_error),
-                                                              conf_int_upper = diff_in_means + (1.96 * std_error),
-                        test_type = test_type, weights_x_used = weights_x_used, weights_y_used = weights_y_used,
-                        x_unweighted_count = length(x[!is.na(x)]), x_na_count = sum(is.na(x)),
-                        y_unweighted_count = length(y[!is.na(y)]), y_na_count = sum(is.na(y))) %>%
+                summary_tbl <- summary_tbl %>% mutate(conf_int_lower = diff_in_means - (2 * std_error),
+                                                      conf_int_upper = diff_in_means + (2 * std_error),
+                                                      test_type = test_type, weights_x_used = weights_x_used, weights_y_used = weights_y_used,
+                                                      x_unweighted_count = length(x[!is.na(x)]), x_na_count = sum(is.na(x)),
+                                                      y_unweighted_count = length(y[!is.na(y)]), y_na_count = sum(is.na(y)),
+                                                      equal_variance = FALSE) %>%
                         select(x_unweighted_count, x_na_count, y_unweighted_count, y_na_count, 
                                mean_x, mean_y, diff_in_means, weights_x_used, weights_y_used, 
-                               test_type, t_stat, p_value, std_error, conf_int_lower, conf_int_upper)
+                               test_type, t_stat, p_value, std_error, equal_variance, conf_int_lower, conf_int_upper)
                 return(summary_tbl)
-        }        
+                }
         
 }
 
@@ -160,43 +150,19 @@ test_means <- function(x, y, weights_x = NULL, weights_y = NULL, weights_are_sta
 # y
 # weights_x <- sample(x = seq(from = 1, to = 2, by = .1), size = length(x), replace = TRUE)
 # weights_y <- sample(x = seq(from = 1, to = 2, by = .1), size = length(y), replace = TRUE)
+# var.equal = FALSE
 # 
-# # un-weighted
-# test_means(x = x, y = y, weights_x = NULL, weights_y = NULL)
-# t.test(x = x, y = y, alternative = "two.sided") %>% tidy()
+# # un-weighted w/ var.equal = FALSE
+# test_means(x = x, y = y, weights_x = NULL, weights_y = NULL, var.equal = FALSE)
+# t.test(x = x, y = y, alternative = "two.sided", var.equal = FALSE) %>% tidy()
 # wtd.t.test(x = x, y = y, weight = NULL, weighty = NULL, samedata = FALSE)
+# 
+# # unweighted w var.equal = TRUE
+# test_means(x = x, y = y, weights_x = NULL, weights_y = NULL, var.equal = TRUE)
+# t.test(x = x, y = y, alternative = "two.sided", var.equal = TRUE) %>% tidy()
 # 
 # # weighted
 # test_means(x = x, y = y, weights_x = weights_x, weights_y = weights_y)
 # wtd.t.test(x = x, y = y, weight = weights_x, weighty = weights_y, samedata = FALSE)
-# 
-# 
-# ##############
-# 
-# 
-# # test dummy variables
-# high_price <- diamonds %>% filter(price >= mean(price)) %>% mutate(fair_dummy = ifelse(cut == "Fair", 1, 0))
-# low_price <- diamonds %>% filter(price < mean(price)) %>% mutate(fair_dummy = ifelse(cut == "Fair", 1, 0))
-# x <- high_price %>% pull(fair_dummy)
-# y <- low_price %>% pull(fair_dummy)
-# weights_x <- sample(x = seq(from = 1, to = 2, by = .1), size = length(x), replace = TRUE)
-# weights_y <- sample(x = seq(from = 1, to = 2, by = .1), size = length(y), replace = TRUE)
-# yates_correction <- FALSE
-# 
-# # un-weighted
-# test_means(x = x, y = y, weights_x = NULL, weights_y = NULL)
-# prop.test(x = c(length(x[x == 1]), length(y[y == 1])), n = c(length(x), length(y)), alternative = "two.sided", correct = yates_correction)
-# t.test(x = x, y = y, alternative = "two.sided") %>% tidy() # slightly higher p-value, but same conf_int
-# wtd.t.test(x = x, y = y, weight = NULL, weighty = NULL, samedata = FALSE)
-# 
-# # weighted
-# test_means(x = x, y = y, weights_x = weights_x, weights_y = weights_y)
-# wtd.t.test(x = x, y = y, weight = weights_x, weighty = weights_y, samedata = FALSE)
-
-
-##############
-
-
-
 
 
--- test_means_of_vars.R
+++ test_means_of_vars.R
@@ -7,13 +7,10 @@
 library(tidyverse)
 library(purrr)
 library(rsample)
-library(caret)
+# library(caret)
 
 # load test_means()
-current_wd <- getwd()
-setwd("H:/R/helper_scripts")
 source("test_means.R")
-setwd(current_wd)
 
 # get attrition data
 # data(attrition)
@@ -35,12 +32,15 @@ setwd(current_wd)
 # attrition_2 <- attrition %>% filter(!(row_number() %in% in_attrition_1))
 # dim(attrition_2)
 
+# note that unweighted t.test with var.equal = TRUE will match ols dummy t.test regression
+
+
 
 #########################################################################
 
 
 # create test_means_of_vars function
-test_means_of_vars <- function(x_tbl, y_tbl, .x, weights_x = NULL, weights_y = NULL, 
+test_means_of_vars <- function(x_tbl, y_tbl, .x, weights_x = NULL, weights_y = NULL, var.equal = FALSE,
                                weights_are_statistical_not_sample_size = TRUE, yates_correction = FALSE) {
         
         # get current_variable_to_test_sym
@@ -70,7 +70,7 @@ test_means_of_vars <- function(x_tbl, y_tbl, .x, weights_x = NULL, weights_y = N
         }
         
         # call test_means
-        output <- test_means(x = x, y = y, weights_x = weights_x, weights_y = weights_y, 
+        output <- test_means(x = x, y = y, weights_x = weights_x, weights_y = weights_y, var.equal = var.equal,
                    weights_are_statistical_not_sample_size = weights_are_statistical_not_sample_size, 
                    yates_correction = yates_correction)
         
@@ -91,13 +91,19 @@ test_means_of_vars <- function(x_tbl, y_tbl, .x, weights_x = NULL, weights_y = N
 # weights_y <- sample(x = seq(from = 1, to = 2, by = .1), size = nrow(y_tbl), replace = TRUE)
 # weights_are_statistical_not_sample_size = TRUE
 # yates_correction = FALSE
+# 
 # current_variable_to_test <- "DistanceFromHome"
 # variables_to_test <- c("Age", "DailyRate", "DistanceFromHome", "HourlyRate")
 # 
 # 
-# map_dfr(.x = variables_to_test, .f = ~ test_means_of_vars(x_tbl = x_tbl, y_tbl = y_tbl, .x))
+# map_dfr(.x = variables_to_test, .f = ~ test_means_of_vars(x_tbl = x_tbl, y_tbl = y_tbl, .x)) %>% data.frame()
+# map_dfr(.x = variables_to_test, .f = ~ test_means_of_vars(x_tbl = x_tbl, y_tbl = y_tbl, .x, var.equal = TRUE)) %>% data.frame()
+# 
 # map_dfr(.x = variables_to_test, .f = ~ test_means_of_vars(x_tbl = x_tbl, y_tbl = y_tbl, .x,
 #                                                           weights_x = weights_x, weights_y = weights_y))
+# map_dfr(.x = variables_to_test, .f = ~ test_means_of_vars(x_tbl = x_tbl, y_tbl = y_tbl, .x, var.equal = TRUE,
+#                                                           weights_x = weights_x, weights_y = weights_y))
+# 
 # # confirm error message that weights needs to be a vector, not a variable name
 # map_dfr(.x = variables_to_test, .f = ~ test_means_of_vars(x_tbl = x_tbl, y_tbl = y_tbl, .x,
 #                                                           weights_x = "weights_x", weights_y = "weights_y"))
