169d26ccc3cb7efbca3079b669df0962af51e2b1
sdevine188 <sdevine188@gmail.com>
2022-05-06 11:51:44 -0400

    updated

--- run_loop.R
+++ run_loop.R
@@ -12,33 +12,23 @@ library(KeyboardSimulator)
 # create run_loop function
 
 # create click_cursor function
-click_cursor <- function(current_second, elapsed_min) {
+click_cursor <- function(current_minute) {
         
         # click mouse
         mouse.click()
         
         # wait a minute
-        Sys.sleep(time = current_second)
+        Sys.sleep(time = 60)
         
         # print minutes elapsed
-        print(elapsed_min)
+        print(current_minute)
 }
 
 # create run_loop function
 run_loop <- function(minutes = 60) {
         
-        # get second_tbl
-        second_tbl <- sample(x = 1:120, size = c(60*60*24), replace = TRUE) %>% tibble(second = .) %>%
-                mutate(elapsed_min = round(cumsum(second) / 60, digits = 1),
-                       target_min = minutes,
-                       time_remaining_to_target_min = target_min - elapsed_min) %>%
-                filter(time_remaining_to_target_min > 0) %>%
-                arrange(desc(time_remaining_to_target_min))
-        second_tbl
-        
         # call move_cursor 
-        walk(.x = 1:nrow(second_tbl), .f = ~ click_cursor(current_second = second_tbl %>% slice(.x) %>% pull(second),
-                                                          elapsed_min = second_tbl %>% slice(.x) %>% pull(elapsed_min)))
+        walk(.x = seq(from = 1, to = minutes, by = 1), .f = ~ click_cursor(current_minute = .x))
 }
 
 
